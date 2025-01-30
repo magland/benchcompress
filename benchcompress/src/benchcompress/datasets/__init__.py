59bea1c9540cac9894317be32479407c9d3556f4
Jeremy Magland <jmagland@flatironinstitute.org>
2025-01-30 18:52:20 -0500

    add ieeg dataset

--- benchcompress/src/benchcompress/datasets/__init__.py
+++ benchcompress/src/benchcompress/datasets/__init__.py
@@ -2,8 +2,15 @@ from .bernoulli import datasets as bernoulli_datasets
 from .gaussian import datasets as gaussian_datasets
 from .electrophysiology import datasets as real_datasets
 from .seismic import datasets as seismic_datasets
+from .ieeg import datasets as ieeg_datasets
 
-datasets_list = [bernoulli_datasets, gaussian_datasets, real_datasets, seismic_datasets]
+datasets_list = [
+    bernoulli_datasets,
+    gaussian_datasets,
+    real_datasets,
+    seismic_datasets,
+    ieeg_datasets,
+]
 
 datasets = []
 for d in datasets_list:
